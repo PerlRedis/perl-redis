388884c25e7a04d6fefb29549acfeb1236496100
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-11-20 23:38:31 +0100

    use redis list and make options configurable

--- scripts/ngram.pl
+++ scripts/ngram.pl
@@ -6,52 +6,75 @@ use strict;
 use lib 'lib';
 use Redis;
 use Getopt::Long;
+use Time::HiRes qw(time);
 use Data::Dump qw(dump);
 
 my $import;
 my $len = 3;
+my $max = 25;
+my $cutoff = 0.75;
+my $debug = 0;
 
 GetOptions(
 	'import=s' => \$import,
 	'len=i'    => \$len,
+	'max=i'    => \$max,
+	'cutoff=f' => \$cutoff,
+	'debug!'   => \$debug,
 ) || die $!;
 
 my $redis = Redis->new;
 
 sub ngram {
 	my ( $string, $callback ) = @_;
-	foreach ( 0 .. length($string) - $len ) {
-		$callback->( substr($string,$_,$len) );
+	$string = lc $string;
+	my $variants = length($string) - $len;
+	foreach ( 0 .. $variants ) {
+		my $ngram = substr($string,$_,$len);
+		$callback->( "N$ngram" );
+		warn "# $_ $ngram\n" if $debug;
 	}
+	return $variants + 1;
 }
 
 if ( $import ) {
+	print STDERR "indexing $import ", -s $import, " bytes with $len ngram ";
 	$redis->flushdb;
-	my $nr = 0;
 	open(my $fh, '<', $import) || die "$import: $!";
 	while(<$fh>) {
 		chomp;
-		$nr++;
-		$redis->set( $nr => $_ );
+		my $nr = $redis->rpush( 'lines' => $_ ) - 1; # -1 to convert into lindex index value
 		ngram $_ => sub { $redis->sadd( $_[0] => $nr ) };
+		warn "# line: $nr $_" if $debug;
+		print STDERR "$nr " if $nr % 1000 == 0;
 	}
-	warn "indexed $import with $nr lines\n";
+	warn "finished ", $redis->llen('lines'), " lines\n";
 }
 
-warn "# enter search:\n";
+print "search> ";
 
-while(<>) {
+while(<STDIN>) {
 	chomp;
 
 	my $score;
-	ngram $_ => sub {
+	my $t = time;
+
+	my $ngrams = ngram $_ => sub {
 		my $ngram = shift;
 		$score->{ $_ }++ foreach $redis->smembers( $ngram );
-		warn "# $ngram ",dump($score);
 	};
 
-	foreach my $nr ( sort { $score->{$a} <=> $score->{$b} } keys %$score ) {
-		printf "%d %s [%d]\n", $nr, $redis->get($nr), $score->{$nr};
+	$t = time - $t;
+
+	my $result = 0;
+
+	foreach my $nr ( sort { $score->{$b} <=> $score->{$a} } keys %$score ) {
+		my $relevance = $score->{$nr} / $ngrams;
+		$result++;
+		last if $result && $result > $max && $relevance < $cutoff;
+		last if $result > $max;
+		printf "%-2d %5d %1.2f %s\n", $result, $nr, $score->{$nr} / $ngrams, $redis->lindex( 'lines' => $nr );
 	}
 
+	printf "%.2f sec ngrams:%d cutoff:%.2f\nsearch> ", $t, $ngrams, $cutoff;
 }
