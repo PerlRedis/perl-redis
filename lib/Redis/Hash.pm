a14e7296960de95d1ddf20bea04a8c506aef7c64
Pedro Melo <melo@simplicidade.org>
2011-01-30 09:02:15 +0000

    Improve Redis::Hash
     * added better synopsis;
     * explain a bit of the internals: we don't use Redis hashes yet;
     * improved test suite.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Redis/Hash.pm
+++ lib/Redis/Hash.pm
@@ -2,7 +2,6 @@ package Redis::Hash;
 
 use strict;
 use warnings;
-
 use Tie::Hash;
 use base qw/Redis Tie::StdHash/;
 
@@ -12,60 +11,87 @@ Redis::Hash - tie perl hashes into Redis
 
 =head1 SYNOPSYS
 
-  tie %name, 'Redis::Hash', 'prefix';
+    ## Create fake hash using keys like 'hash_prefix:KEY'
+    tie %my_hash, 'Redis::Hash', 'hash_prefix', @Redis_new_parameters;
+    
+    ## Treat the entire Redis database as a hash
+    tie %my_hash, 'Redis::Hash', undef, @Redis_new_parameters;
+    
+    $value = $my_list{$key};
+    $my_list{$key} = $value;
+    
+    @keys   = keys %my_hash;
+    @values = values %my_hash;
+    
+    %my_hash = reverse %my_hash;
+    
+    %my_hash = ();
 
-  my $o = tie %foobar, 'Redis::Hash', 'foobar';
-  print $o->info->{used_memory}; # or any redis command
+
+=head1 DESCRIPTION
+
+Ties a Perl hash to Redis. Note that it doesn't use Redis Hashes, but
+implements a fake hash using regular keys like "prefix:KEY".
+
+If no C<prefix> is given, it will tie the entire Redis database
+as a hash.
+
+Future versions will also allow you to use real Redis hash structures.
+
 
 =cut
 
-# mandatory methods
+
 sub TIEHASH {
-  my ($class, $name) = @_;
-  my $self = Redis->new;
-  $name .= ':' if $name;
-  $self->{name} = $name || '';
-  bless $self => $class;
+  my ($class, $prefix, @rest) = @_;
+  my $self = $class->new(@rest);
+
+  $self->{prefix} = $prefix ? "$prefix:" : '';
+
+  return $self;
 }
 
 sub STORE {
   my ($self, $key, $value) = @_;
-  $self->set($self->{name} . $key, $value);
+  $self->set($self->{prefix} . $key, $value);
 }
 
 sub FETCH {
   my ($self, $key) = @_;
-  $self->get($self->{name} . $key);
+  $self->get($self->{prefix} . $key);
 }
 
 sub FIRSTKEY {
   my $self = shift;
-  $self->{keys} = [$self->keys($self->{name} . '*')];
+  $self->{prefix_keys} = [$self->keys($self->{prefix} . '*')];
   $self->NEXTKEY;
 }
 
 sub NEXTKEY {
   my $self = shift;
-  my $key  = shift @{$self->{keys}} || return;
-  my $name = $self->{name};
-  $key =~ s{^$name}{} || warn "can't strip $name from $key";
+
+  my $key = shift @{$self->{prefix_keys}};
+  return unless defined $key;
+
+  my $p = $self->{prefix};
+  $key =~ s/^$p// if $p;
   return $key;
 }
 
 sub EXISTS {
   my ($self, $key) = @_;
-  $self->exists($self->{name} . $key);
+  $self->exists($self->{prefix} . $key);
 }
 
 sub DELETE {
   my ($self, $key) = @_;
-  $self->del($self->{name} . $key);
+  $self->del($self->{prefix} . $key);
 }
 
 sub CLEAR {
   my ($self) = @_;
-  $self->del($_) foreach ($self->keys($self->{name} . '*'));
-  $self->{keys} = [];
+  $self->del($_) for $self->keys($self->{prefix} . '*');
+  $self->{prefix_keys} = [];
 }
 
 1;
--- t/20-Redis-Hash.t
+++ t/20-Redis-Hash.t
@@ -1,28 +1,67 @@
-#!/usr/bin/perl
+#!perl
 
 use warnings;
 use strict;
+use Test::More;
+use Test::Deep;
+use Redis::Hash;
+use lib 't/tlib';
+use Test::SpawnRedisServer;
 
-use Test::More tests => 8;
-use lib 'lib';
+my ($guard, $srv) = redis();
 
-BEGIN {
-  use_ok('Redis::Hash');
-}
+## Setup
+my %my_hash;
+ok(my $redis = tie(%my_hash, 'Redis::Hash', 'my_hash', server => $srv),
+  'tied to our test redis-server');
+ok($redis->ping, 'pinged fine');
+isa_ok($redis, 'Redis::Hash');
 
-ok(my $o = tie(my %h, 'Redis::Hash', 'test-redis-hash'), 'tie');
 
-isa_ok($o, 'Redis::Hash');
+## Direct access
+subtest 'direct access' => sub {
+  %my_hash = ();
+  cmp_deeply(\%my_hash, {}, 'empty list ok');
 
-$o->CLEAR();
+  %my_hash = (a => 'foo', b => 'bar', c => 'baz');
+  cmp_deeply(
+    \%my_hash,
+    {a => 'foo', b => 'bar', c => 'baz'},
+    'Set multiple values ok'
+  );
 
-ok(!keys %h, 'empty');
+  $my_hash{b} = 'BAR';
+  cmp_deeply(
+    \%my_hash,
+    {a => 'foo', b => 'BAR', c => 'baz'},
+    'Set single value ok'
+  );
 
-ok(%h = ('foo' => 42, 'bar' => 1, 'baz' => 99), '=');
+  is($my_hash{c}++, 'baz', 'get single value ok');
+  is(++$my_hash{c}, 'bbb', '... even with post/pre-increments');
+};
 
-is_deeply([sort keys %h], ['bar', 'baz', 'foo'], 'keys');
 
-is_deeply(\%h, {bar => 1, baz => 99, foo => 42,}, 'structure');
+## Hash functions
+subtest 'hash functions' => sub {
+  ok(my @keys = keys(%my_hash), 'keys ok');
+  cmp_deeply(\@keys, bag(qw( a b c )), '... resulting list as expected');
 
-ok(my $mem = $o->info->{used_memory}, 'info');
+  ok(my @values = values(%my_hash), 'values ok');
+  cmp_deeply(\@values, bag(qw( foo BAR bbb )),
+    '... resulting list as expected');
 
+  %my_hash = reverse %my_hash;
+  cmp_deeply(
+    \%my_hash,
+    {foo => 'a', BAR => 'b', bbb => 'c'},
+    'reverse() worked'
+  );
+};
+
+
+## Cleanup
+%my_hash = ();
+cmp_deeply(\%my_hash, {}, 'empty list ok');
+
+done_testing();
