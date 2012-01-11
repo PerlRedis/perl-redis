e723e47c17c97a4556741443baad152e2a9c1c00
Pedro Melo <melo@simplicidade.org>
2012-01-11 16:45:07 +0000

    Tweak benchmark script a bit:
     * use perl from PATH;
     * include some big payloads;
     * run benchmarks at least 5 seconds.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- scripts/redis-benchmark.pl
+++ scripts/redis-benchmark.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use warnings;
 use strict;
@@ -13,17 +13,21 @@ my %hash;
 tie %hash, 'Redis::Hash', 'hash';
 
 my $i = 0;
+my $big = 'a' x 16_000;
+my @many = (1..1000);
 
 timethese( -5, {
 	'00_ping'	=> sub { $r->ping },
 	'10_set'	=> sub { $r->set( 'foo', $i++ ) },
+	'12_set_big'	=> sub { $r->set( 'foo-big', $big ) },
 	'11_set_r'	=> sub { $r->set( 'bench-' . rand(), rand() ) },
 	'20_get'	=> sub { $r->get( 'foo' ) },
 	'21_get_r'	=> sub { $r->get( 'bench-' . rand() ) },
+	'22_get_big'	=> sub { $r->get( 'foo-big' ) },
 	'30_incr'	=> sub { $r->incr( 'counter' ) },
 	'30_incr_r'	=> sub { $r->incr( 'bench-' . rand() ) },
 	'40_lpush'	=> sub { $r->lpush( 'mylist', 'bar' ) },
-	'40_lpush'	=> sub { $r->lpush( 'mylist', 'bar' ) },
+	'41_lpush_many'	=> sub { $r->lpush( 'mylist', @many ) },
 	'50_lpop'	=> sub { $r->lpop( 'mylist' ) },
 	'90_h_set' => sub { $hash{ 'test' . rand() } = rand() },
 	'90_h_get' => sub { my $a = $hash{ 'test' . rand() }; },
