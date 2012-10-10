2aea92350fa1a6bf06fc75d7852e530059c3224c
Pedro Melo <melo@simplicidade.org>
2012-10-10 12:44:11 +0100

    Build results of 8db2040 (on master)

--- lib/Redis/Hash.pm
+++ lib/Redis/Hash.pm
@@ -1,7 +1,7 @@
 package Redis::Hash;
 
 # ABSTRACT: tie Perl hashes to Redis hashes
-our $VERSION = '1.954'; # VERSION
+our $VERSION = '1.955'; # VERSION
 our $AUTHORITY = 'cpan:MELO'; # AUTHORITY
 
 use strict;
@@ -79,7 +79,7 @@ Redis::Hash - tie Perl hashes to Redis hashes
 
 =head1 VERSION
 
-version 1.954
+version 1.955
 
 =head1 DESCRIPTION
 
