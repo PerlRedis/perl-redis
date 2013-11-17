53378043482bd51203e76b8bfdbaf357b39cc9f4
Damien Krotkine <dams@zarb.org>
2013-11-17 21:36:12 +0100

    reuse addr

--- tools/benchmarks/readline_vs_sysread_vs_recv/server-generator.pl
+++ tools/benchmarks/readline_vs_sysread_vs_recv/server-generator.pl
@@ -9,7 +9,8 @@ my $sock = IO::Socket::INET->new(
     Listen    => 5,
     LocalAddr => 'localhost',
     LocalPort => 1234,
-    Proto     => 'tcp'
+    Proto     => 'tcp',
+    ReuseAddr => 1,
 );
 
 die $! unless $sock;
@@ -19,10 +20,10 @@ while (my $client = $sock->accept()) {
     my $line = <$client>;
     chomp $line;
 
-    my ($cnt, $len) = split(',', $line);
-    next unless $cnt || $len;
+#    my ($cnt, $len) = split(',', $line);
+#    next unless $cnt || $len;
 
-    for (my $i = 1; $i <= $cnt; ++$i) {
-        print $client '.' x $len, "\n";
-    }
+#    for (my $i = 1; $i <= $cnt; ++$i) {
+#        print $client '.' x $len, "\n";
+#    }
 }
